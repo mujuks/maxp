e6ae755226339e4def65fbbdf35bb8f866c1863c
Maxwell mujeni <maxwellmujeni@gmail.com>
2026-08-06 19:03:28 +0300

    ScrollTop
    ScrollTop

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
+import ScrollToTop from "./Components/utils/ScrollTop";
+
 
 
 function App() {
   return (
     <BrowserRouter>
-
+    <ScrollToTop />
       <Routes>
+        
 
         <Route path="/"  element={<Home />} />
         <Route path="/about" element={<About />} />
